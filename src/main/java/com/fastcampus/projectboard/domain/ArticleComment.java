5e2023a2bb4d2dd543bf9aa65a4559baa38c873d
user <ssizzle88@gmail.com>
2023-04-25 20:45:25 +0900

    #8-도메인 클래스설계(게시글, 댓글)
    주석 수정

--- src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
+++ src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
@@ -5,10 +5,11 @@ import java.time.LocalDateTime;
 public class ArticleComment {
     private Long id;
     private Article article; // 게시글(ID)
-    private String content; // 본자
+    private String content; // 본문
 
     private LocalDateTime createdAt; //생성일시
     private String createdBy; //생성자
     private LocalDateTime modifiedAt; //수정일자
     private String modifiedBy; //수정자
+
 }
